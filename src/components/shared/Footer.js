4e48daeb5d91fa98d192989d4102a9a0fee1afbd
shahrokh <shahrokhdallavi@gmail.com>
2024-08-11 17:57:04 +0330

    crypto web final commit

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='flex justify-center bg-secondaryColor'>
             <div className="py-2 px-8">
-                <p className="font-extralight text-sm text-white">© 1403 . 2024 . Develope by <span className="font-medium text-primaryColor">Shahrokh Dallavi</span></p>
+                <p className="font-extralight text-sm text-white">© 1403 . 2024 . Develope by <a href='tel:09333203673' title='phone number' className="font-medium text-primaryColor">Shahrokh Dallavi</a></p>
             </div>
         </div>
     );
